141376cf0a1494659ebcd7ab29007ba38462af4f
manoj <manoj.singh8890@gmail.com>
2017-10-13 19:55:07 +0530

    Inital Commit

---
+++ Debug_tool/src/main/java/com/hsc/Debug_tool/CustomizationBean.java
@@ -0,0 +1,14 @@
+package com.hsc.Debug_tool;
+
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomizationBean implements EmbeddedServletContainerCustomizer {
+    @Override
+    public void customize(ConfigurableEmbeddedServletContainer container) {
+        container.setPort(8080);
+        // Use port of your choice that is available
+    }
+}
